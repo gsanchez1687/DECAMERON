5865760043b2c8c8501e1f1b0824fa837ee5119e
Guiillermo Enrique <gsanchez1687@gmail.com>
2024-07-15 16:54:22 -0500

    eliminando los ajustes de colores

--- Admin/components/layout/customizer.vue
+++ Admin/components/layout/customizer.vue
@@ -1,88 +1,8 @@
 <template>  
-  <div class="customizer-wrap" :class="open?'open' : ''">
-    <div class="customizer-links"  @click="open = true">
-      <Icon name="material-symbols:settings-outline" />
-    </div>
-    <div class="customizer-contain custom-scrollbar">
-        <div class="setting-back">
-            <Icon name="ic:round-close" @click="open = false"/>
-        </div>
-        <div class="layouts-settings">
-            <div class="customizer-title">
-                <h6 class="color-4">Layout type</h6>
-            </div>
-            <div class="option-setting">
-                <span>Light</span>
-                <label class="switch">
-                    <input type="checkbox" name="chk1" value="option" @input="check()" class="setting-check"><span class="switch-state"></span>
-                </label>
-                <span>Dark</span>
-            </div>
-        </div>
-        <div class="layouts-settings">
-            <div class="customizer-title">
-                <h6 class="color-4">Layout Direction</h6>
-            </div>
-            <div class="option-setting">
-                <span>LTR</span>
-                <label class="switch">
-                    <input type="checkbox" name="chk2" value="option"  @input="layout()" class="setting-check1"><span class="switch-state"></span>
-                </label>
-                <span>RTL</span>
-            </div>
-        </div>
-        <div class="layouts-settings">
-            <div class="customizer-title">
-                <h6 class="color-4">Unlimited Color</h6>
-            </div>
-             <div class="option-setting unlimited-color-layout">
-                <div class="form-group">
-                    <label for="ColorPicker1">color 1</label>
-                    <input id="ColorPicker1" v-model="primary" @change="setcolor()" type="color"  name="Default">
-                </div>
-                <div class="form-group">
-                    <label for="ColorPicker2">color 2</label>
-                    <input id="ColorPicker2" v-model="secondary" type="color" @change="setcolor()" name="Default">
-                </div> 
-            </div> 
-        </div>
-    </div>
-</div>
+  
 </template>
 
 <script setup lang="ts">
-    import {usecustomizerStore} from '@/store/costomizer'
-let primary = ref<string>('#1e90ff');
-let secondary = ref<string>('#3742fa');
-let open = ref<boolean>(false)
-let dark = ref<boolean>(false)
-let dir = ref<boolean>(false)
-let store = usecustomizerStore();
-let color = useCookie('color');
-function setcolor(){
-    color.value = {primary : primary.value , secondary : secondary.value}
-    store.setcolor(color.value)
-}
-function check(){
-    dark.value = !dark.value
-    if(dark.value == true){
-        document.body.classList.add('dark-layout')
-    }
-    else{
-        document.body.classList.remove('dark-layout')
-    }
-}
-function layout(){
-    dir.value = !dir.value
-    if(dir.value == true){
-        document.body.classList.add('rtl')
-        document.documentElement.dir = "rtl"
-    }
-    else{
-        document.body.classList.remove('rtl')
-        document.documentElement.dir = "ltr"
-    }
-}
 
 </script>
 

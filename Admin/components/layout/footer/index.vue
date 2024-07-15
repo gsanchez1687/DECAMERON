cc63e4e7fca47abf3e88ce9f5df621a8b1bfafd8
Guiillermo Enrique <gsanchez1687@gmail.com>
2024-07-15 09:13:06 -0500

    fix footer

--- Admin/components/layout/footer/index.vue
+++ Admin/components/layout/footer/index.vue
@@ -3,7 +3,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-6 footer-copyright">
-                <p class="mb-0">Copyright 2023 © Sheltos All rights reserved.</p>
+                <p class="mb-0">Copyright 2024 © Sheltos All rights reserved.</p>
             </div>
             <div class="col-md-6">
                 <p class="float-end mb-0">Developed with <i class="fa fa-heart font-danger"></i></p>
